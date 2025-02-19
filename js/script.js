a3af945eb350d1db551dd237dcc3cd799395de54
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-02-19 19:49:20 +0200

    ss

--- js/script.js
+++ js/script.js
@@ -4,10 +4,21 @@ while(number <= 10) {
     console.log(number);
     number += 1
 }
+console.log("--------------------------------------------------");
 // 2
 for(let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
+    if (i % 2 !== 0) {
         continue;
     }
     console.log(i);
+}
+console.log("---------------------------------------------------");
+// 3
+for (let i = 1; i <= 10; i++) {
+    console.log(`7 * ${i} = ${7 * i}`);
+}
+console.log("------------------------------------------------------");
+// 6
+for (let n = 12; n <= 12; n) {
+    console.log(n);
 }
