fbe132a23edfc0874a81e0bd84d89175c3754c2c
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-02-19 18:48:12 +0200

    :)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // 1
-// let number = 1
-// while(number <= 10) {
-//     console.log(number);
-//     number = +1
-// }
+let number = 1
+while(number <= 10) {
+    console.log(number);
+    number += 1
+}
+// 2
+for(let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log(i);
+}
