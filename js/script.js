22401a408a1dbb958c46f7b2be3c0f3068557ca7
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-11-29 10:55:12 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,35 +1,12 @@
-// 1
-let number = 1
-while(number <= 10) {
-    console.log(number);
-    number += 1
-}
-console.log("--------------------------------------------------");
-// 2
-for(let i = 1; i <= 20; i++) {
-    if (i % 2 !== 0) {
-        continue;
-    }
-    console.log(i);
-}
-console.log("---------------------------------------------------");
-// 3
-for (let i = 1; i <= 10; i++) {
-    console.log(`7 * ${i} = ${7 * i}`);
-}
-console.log("------------------------------------------------------");
-// 6
-let n = 12;
-let i = 1;
-while (i <= n) {
-    console.log(i);
-    i += 1;
-}
-console.log("------------------------------------------------------");
-// 7
-for (let i = 1; i <= 20; i++) {
-    if (i % 3 !== 0) {
-        continue;
-    }
-    console.log(i);
-}
+import { add, multiply } from "./math.js";
+
+const result1 = add(5, 3);
+const result2 = multiply(4, 7);
+
+console.log("Sum:", result1);
+console.log("Product:", result2);
+
+document.body.innerHTML = `
+  <p>5 + 3 = ${result1}</p>
+  <p>4 × 7 = ${result2}</p>
+`;
